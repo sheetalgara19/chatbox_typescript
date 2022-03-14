045a3dde98761f57564e451ceb44d5573be2f553
sheetalg <sheetal_gara@ultimatesoftware.com>
2022-03-13 19:08:11 -0700

    Reference of <App/> in index.tsx replaced by <Page1/>

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Page1 from "./components/Page1";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Page1/>
   </React.StrictMode>,
   document.getElementById('root')
 );
